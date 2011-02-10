f0453440fbc4783df35b116df5b4b4c93a062806
David Santiago <david.santiago@gmail.com>
2011-02-10 13:45:41 -0600

    Fixed sonatype repo to be release, not snapshot.

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "A task for controlling pallet."
   :dependencies [[clojure "1.2.0"]
                  [org.cloudhoist/pallet "0.4.0-beta-1"]]
-  :repositories {"sonatype-snapshot" "https://oss.sonatype.org/content/repositories/snapshots/"}
+  :repositories {"sonatype-release" "https://oss.sonatype.org/content/repositories/releases/"}
   :tasks [cake-pallet.tasks])
