c2384292bfca616416a1a9adab6f24fb2f3a19c4
Hugo Duncan <hugo@hugoduncan.org>
2011-02-12 10:37:27 -0500

    Allow use of cake's parsed options for pallet task options
    This allows use of cake's native syntax for parsing options to a task,
    making these equivalent:
        cake pallet --P=nl nodes
        cake pallet nodes -- -P nl

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject cake-pallet "0.4.0-beta-1"
+(defproject cake-pallet "0.4.0-SNAPSHOT"
   :description "A task for controlling pallet."
   :dependencies [[clojure "1.2.0"]
-                 [org.cloudhoist/pallet "0.4.0-beta-1"]]
+                 [org.cloudhoist/pallet "0.4.0-SNAPSHOT"]]
   :tasks [cake-pallet.tasks])
--- src/cake_pallet/tasks.clj
+++ src/cake_pallet/tasks.clj
@@ -1,19 +1,28 @@
 (ns cake-pallet.tasks
   "A cake task for controlling pallet.
 
-   Adapted from Hugo Duncan's pallet-lein at http://github.com/hugoduncan/pallet-lein."
+   Adapted from Hugo Duncan's pallet-lein at
+   http://github.com/pallet/pallet-lein."
   (:use cake cake.core))
 
 (deftask pallet
   "Launch pallet tasks from the command line. Use 'cake pallet help' for tasks."
   (bake (:require [clojure.string :as str])
         (:require pallet.main)
-        [options (concat (:pallet *opts*)
-                         (rest (drop-while #(not= "--" %) *command-line-args*)))]
-        (do
+        [options (concat
+                  (:pallet *opts*)
+                  (rest (drop-while #(not= "--" %) *command-line-args*)))]
+        (let [parsed-options (mapcat
+                              #(vector
+                                (str "-" (name (key %))) (str/join "," (val %)))
+                              (dissoc *opts* :pallet))
+              args (concat
+                    ["-project-options" (pr-str *project*)]
+                    parsed-options
+                    options)]
           (try
             (require 'pallet.main)
-            (pallet.main/pallet-task (concat ["-project-options" (pr-str *project*)] options))
+            (pallet.main/pallet-task args)
             (catch java.io.FileNotFoundException e
               (println "Error loading pallet: " (.getMessage e))
               (println "You need to have pallet as a project dependency")
