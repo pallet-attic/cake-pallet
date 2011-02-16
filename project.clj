598b5cb720d60e403cdca46be10e21b24763880f
Hugo Duncan <hugo@hugoduncan.org>
2011-02-16 16:09:33 -0500

    Update version to 0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cake-pallet "0.4.0-SNAPSHOT"
+(defproject cake-pallet "0.4.0"
   :description "A task for controlling pallet."
   :dependencies [[clojure "1.2.0"]
                  [org.cloudhoist/pallet "0.4.0"]]
